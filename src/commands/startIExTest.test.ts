bf5e949e539d5dbce02c42a24c111794893052cb
Scott Ming <TheRealScottMing@Gmail.com>
2022-11-24 15:41:10 +0800

    Fix typo

--- src/commands/startIExTest.test.ts
+++ src/commands/startIExTest.test.ts
@@ -3,7 +3,7 @@ import { populateStartText } from './startIExTest';
 const defaultStartText = 'TestIex.start...';
 
 describe(`populateStartText/3`, () => {
-  test('it should not return text that modify the working dir when not umbrella app', () => {
+  test('it should not return text that modify the working dir when not in a umbrella app', () => {
     const openedFileName = '/demo/file_test.exs';
     const cwdFun = () => '/demo';
 
@@ -11,7 +11,7 @@ describe(`populateStartText/3`, () => {
     expect(result).toMatch(new RegExp(`^${defaultStartText}`));
   });
 
-  test('it should return text that `cd` to working dir when in a umbrella app', () => {
+  test('it should return text that `cd` to child app dir when in a umbrella app', () => {
     const childAppPath = '/umbrella_demo/apps/child_app';
     const openedFileName = childAppPath + '/file_test.exs';
     const cwdFun = () => '/umbrella_demo';
