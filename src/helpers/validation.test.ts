0ebe676a9ae2cd50e6565ff2fff6bf35bfe32db3
Scott Ming <therealscottming@gmail.com>
2023-07-09 19:52:50 +0800

    Support elixir 1.15 when using asdf

--- src/helpers/validation.test.ts
+++ src/helpers/validation.test.ts
@@ -1,3 +1,4 @@
+import * as Validation from '../helpers/validations';
 import { populateStartText } from '../helpers/validations';
 
 const EXTENSION_PATH = '/home/username/.vscode/extensions/vscode-elixir-test-in-iex/';
@@ -26,5 +27,35 @@ describe(`populateStartText/3`, () => {
 
     const result = populateStartText(openedFileName, EXTENSION_PATH, cwdFun);
     expect(result).toMatch(`cd ${childAppPath}`);
+  });
+});
+
+describe(`populateStartText/3 for Elixir 1.15`, () => {
+  test('returns false when Elixir version is bigger than 1.15', () => {
+    const mockAsdfElixirVersion = jest.spyOn(Validation, 'asdfElixirVersion');
+    let result = 'elixir          1.15.4-otp-25   /Users/scottming/Code/.tool-versions';
+    mockAsdfElixirVersion.mockReturnValue(result);
+    expect(Validation.smallerThan115('/demo')).toBe(false);
+  });
+  test('returns true when Elixir version is smaller than 1.15', () => {
+    const mockAsdfElixirVersion = jest.spyOn(Validation, 'asdfElixirVersion');
+    let result = 'elixir          1.14.4-otp-25   /Users/scottming/Code/.tool-versions';
+    mockAsdfElixirVersion.mockReturnValue(result);
+    expect(Validation.smallerThan115('/demo')).toBe(true);
+  });
+
+  test('returns false when Elixir version is not found', () => {
+    const mockAsdfElixirVersion = jest.spyOn(Validation, 'asdfElixirVersion');
+    let result =
+      'elixir          ______          No version is set. Run "asdf <global|shell|local> elixir <version>"';
+    mockAsdfElixirVersion.mockReturnValue(result);
+    expect(Validation.smallerThan115('/demo')).toBe(false);
+  });
+
+  test('returns false when asdf command is not found', () => {
+    const mockAsdfElixirVersion = jest.spyOn(Validation, 'asdfElixirVersion');
+    let result = null;
+    mockAsdfElixirVersion.mockReturnValue(result);
+    expect(Validation.smallerThan115('/demo')).toBe(false);
   });
 });
